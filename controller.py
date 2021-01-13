978994a252b6636a42bf330d209e13f78562c129
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-01-13 16:57:54 -0700

    found some bug with the output fuzzing

--- controller.py
+++ controller.py
@@ -14,20 +14,21 @@ if BATCH:
         )
         result = float(result.strip())
         answers.append(result)
-        print(result)
+        print(test_mol, result)
     for answer, test_mol in zip(answers, test_mols):
+        # I don't think options are being passed correctly here, all the values seem the same
         prediction = client.containers.run(
-            "mmh42/sampl-test:0.1", "--fuzz", test_mol, auto_remove=True
+            "mmh42/sampl-test:0.1", test_mol, "--fuzz", auto_remove=True
         )
         prediction = float(prediction.strip())
-        print(prediction)
+        print(test_mol, prediction)
         errors.append(prediction - result)
 
     # RMSE
     RMSE = sum([_ ** 2 for _ in errors]) / len(errors)
     print(f"score: {RMSE}")
 
-
-result = client.containers.run("mmh42/sampl-test:0.1", "CCCC", auto_remove=True)
-result = float(result.strip())
-print(result)
+else:
+    result = client.containers.run("mmh42/sampl-test:0.1", "CCCC", auto_remove=True)
+    result = float(result.strip())
+    print(result)
