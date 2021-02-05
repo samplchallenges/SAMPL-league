dd53aab9ccaa9c16ee45875c661ee260b7a50ea3
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-02-05 10:40:10 -0700

    switching to platform hooks, its the newer and better way

---
+++ .platform/hooks/postdeploy/01_django_migrate.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ $EB_IS_COMMAND_LEADER == "true" ]];
+then
+	source "$PYTHONPATH/activate" && {
+	# log which migrations have already been applied
+	python manage.py showmigrations;
+	# migrate
+	python manage.py migrate --noinput;
+	# bootstrap superuser
+	python manage.py mysuperuser
+	# collect static 
+	python manage.py collectstatic --noinput
+	}
+else
+  echo "this instance is NOT the leader";
+fi
