c0a593a678c12a0e3f925618bb80b67635d027f8
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-02-08 07:31:54 -0700

    sym link not working, hardcoding path

--- app/.platform/hooks/predeploy/01_https_setup.sh
+++ app/.platform/hooks/predeploy/01_https_setup.sh
@@ -10,6 +10,3 @@ sudo yum install -y certbot
 
 echo "running certbot"
 sudo certbot certonly --debug --non-interactive --email mhenry5@uci.edu --agree-tos --standalone --domains app.samplchallenges.org --keep-until-expiring --pre-hook \"sudo service nginx stop\" --post-hook \"sudo service nginx start\"
-
-echo "linking cert"
-sudo ln -sf /etc/letsencrypt/live/app.samplchallenges.org /etc/letsencrypt/live/ebcert
