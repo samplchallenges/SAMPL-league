be7b9e6004fad10a17e6681b0ad7c987c6dfffcc
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-02-18 13:44:08 -0700

    testing the score method

---
+++ attic/score-container/score.py
@@ -0,0 +1,33 @@
+import click
+
+
+@click.command()
+@click.argument("path", type=click.Path(exists=True))
+def score(path):
+    from pathlib import Path
+
+    p = Path(path)
+
+    # Even if the sorting is not "right" it will be equally wrong
+    predictions = sorted(list(p.glob("prediction_mol_*")))
+    measurements = sorted(list(p.glob("measurement_mol_*")))
+
+    assert len(predictions) == len(measurements)
+
+    for prediction, measurement in zip(predictions, measurements):
+        with open(prediction, "r") as prediction_file, open(
+            measurement, "r"
+        ) as measurement_file:
+            prediction_val, measurement_val = (
+                float(prediction_file.readline().strip()),
+                float(measurement_file.readline().strip()),
+            )
+        score = (prediction_val - measurement_val) ** 2
+        mol_name = prediction.name[len("prediction_"):]
+        score_file_name = prediction.parent.joinpath(f"score_{mol_name}")
+        with open(score_file_name, "w") as score_file:
+            score_file.write(score)
+
+
+if __name__ == "__main__":
+    score()
