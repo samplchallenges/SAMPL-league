704d9c1fec2f2baf9585923ee268f8003d340d0e
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-03-02 12:50:20 -0700

    added some logging, now to add dask

--- attic/ref2.py
+++ attic/ref2.py
@@ -1,6 +1,11 @@
+import logging
+import os
 from pathlib import Path
 
 from dask.distributed import Client
+
+LOGLEVEL = os.environ.get("LOGLEVEL", "WARNING").upper()
+logging.basicConfig(level=LOGLEVEL)
 
 
 class Submission:
@@ -10,6 +15,9 @@ class Submission:
         self.args = self.challenge_data["args"]
         self.image = self.challenge_data["image"]
         self.scoring_image = self.challenge_data["scoreing_image"]
+        self.private_data = self.challenge_data["PRIVATE_DATA"]
+        self.public_data = self.challenge_data["PUBLIC_DATA"]
+        self.submission_dir = self.challenge_data["SUBMISSION_DIR"]
 
     def prep(self):
         self._pull_image()
@@ -26,11 +34,13 @@ class Submission:
         container = self.client.images.pull(self.image)
         self.image_hash = container.attrs.get("RepoDigests")[0]
 
-    def _get_mol_list(self, mol_path):
+    @staticmethod
+    def _get_mol_list(mol_path):
         if mol_path.exists() and mol_path.is_dir():
             mol_path = mol_path.resolve()
             mols = list(mol_path.glob("mol_*"))
-            self.mols = mols
+            # return or set self.mols = mols ?
+            return mols
         else:
             raise OSError("does not exist or not a directory")
 
@@ -38,26 +48,31 @@ class Submission:
         run_id = str(self.image_hash)
         # slice off sha:
         run_id = run_id.split(":")[-1]
-        self.output_dir_public = SUBMISSION_DIR.joinpath(run_id, "public").mkdir(
-            parents=True, exist_ok=True
-        )
-        self.output_dir_public = SUBMISSION_DIR.joinpath(run_id, "private").mkdir(
-            parents=True, exist_ok=True
-        )
 
-    def _run(self, input_mol, output_dir, cache):
+        # would be nice to have
+        # name =  dir.joinpath("foo").mkdir() return path
+
+        self.output_dir_public = self.submission_dir.joinpath(run_id, "public")
+        self.output_dir_public.mkdir(parents=True, exist_ok=True)
+
+        self.output_dir_private = self.submission_dir.joinpath(run_id, "private")
+        self.output_dir_private.mkdir(parents=True, exist_ok=True)
+
+    def _run(self, input_mol, output_dir, use_cache=True):
+        if use_cache is False:
+            raise NotImplementedError
         mol_name = input_mol.name
         try:
             with open(output_dir.joinpath(f"score_{mol_name}"), "r") as score_file:
                 score = score_file.readline().strip()
-            print(f"cache hit {mol_name}")
+            logging.debug(f"cache hit {mol_name}")
             return float(score)
         except (FileNotFoundError):
             with open(input_mol, "r") as mol_file:
                 mol = mol_file.readline().strip()
 
             command = [mol] + self.args
-            print(f"running command: {command} on {mol_name} {mol}")
+            logging.debug(f"running command: {command} on {mol_name} {mol}")
             result = self.client.containers.run(self.image, command, auto_remove=True)
             # Will want to catch an excpetion here and pass the log if done in public
             result = float(result.strip())
@@ -66,6 +81,9 @@ class Submission:
             return result
 
     def test_container(self):
+        mol_list = self._get_mol_list(self.public_data)
+        for mol in mol_list:
+            self._run(mol, self.output_dir_public, use_cache=True)
         # run on public data + score
         # also return the logs publicly
         pass
@@ -93,6 +111,9 @@ SUBMISSION_DIR = Path("data-submissions").resolve()
 # Mock things that we should pull from DB as dictionary
 CHALLENGE_DB = {
     0: {
+        "PUBLIC_DATA": PUBLIC_DATA,
+        "PRIVATE_DATA": PRIVATE_DATA,
+        "SUBMISSION_DIR": SUBMISSION_DIR,
         "image": "localhost:5000/mmh42/sampl-test:0.1",
         "scoreing_image": "mmh42/score-submission:0.1",
         "challenge_name": "LogP",
@@ -103,6 +124,6 @@ CHALLENGE_DB = {
 challenge_id = 0
 submission = Submission(challenge_id)
 submission.prep()
-# submission.test_container()
+submission.test_container()
 # submission.run()
 # submission.grade()
