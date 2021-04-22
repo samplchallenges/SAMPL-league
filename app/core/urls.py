89889d495074ebc3e2fb0e2ccefadddbfc1ff920
Braxton <robbason@users.noreply.github.com>
2021-04-22 13:18:00 -0400

    issue-13: clone doesn't create a new object until you save (#34)

--- app/core/urls.py
+++ app/core/urls.py
@@ -19,9 +19,14 @@ urlpatterns = [
     path("challenge/", ChallengeList.as_view(), name="challenge-list"),
     path("challenge/<int:pk>/", ChallengeDetail.as_view(), name="challenge-detail"),
     path("submission/", SubmissionList.as_view(), name="submission-list"),
-    path("submission/add/", edit_submission_view, name="submission-add"),
-    path("submission/<int:pk>/clone/", clone_submission_view, name="submission-clone"),
     path("submission/<int:pk>/", SubmissionDetail.as_view(), name="submission-detail"),
+    path("submission/add/", edit_submission_view, name="submission-add"),
+    path(
+        "submission/<int:pk>/clone/",
+        edit_submission_view,
+        name="submission-clone",
+        kwargs={"clone": True},
+    ),
     path(
         "submission/<int:pk>/edit/",
         edit_submission_view,
