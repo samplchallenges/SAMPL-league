24a8a78c5d5cb906e78ab210d2b897d63901cbf6
Braxton Robbason <braxton.robbason@gmail.com>
2021-04-27 16:34:26 -0400

    issue-31: test management command

--- app/core/management/commands/sample_data.py
+++ app/core/management/commands/sample_data.py
@@ -1,5 +1,3 @@
-import os
-
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 from django.utils import timezone
---
+++ app/tests/test_management.py
@@ -0,0 +1,22 @@
+from io import StringIO
+
+import pytest
+
+from django.core.management import call_command
+
+from core import models
+
+
+@pytest.mark.django_db
+def test_sample_data():
+    out = StringIO()
+    call_command("sample_data", stdout=out, stderr=StringIO())
+
+    challenge = models.Challenge.objects.get(name="Milo")
+    assert challenge.container_set.count() == 2
+    assert challenge.submission_set.count() == 1
+    submission = models.Submission.objects.get()
+    assert submission.submissionrun_set.count() == 1
+    assert models.InputElement.objects.all().count() == 6
+    assert models.InputType.objects.all().count() == 1
+    assert models.InputValue.objects.all().count() == 6
