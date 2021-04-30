18842be2085a93ca2f1387a85b01bf05cee9500e
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-04-29 17:33:46 -0700

    close to getting this to work, still need to figure out how to get the values in the DB

--- app/referee/tasks.py
+++ app/referee/tasks.py
@@ -18,15 +18,60 @@ def fire_off_tasks(submission_id, elements):
 
 def run_submission_element(submission_id, element):
     print("fired")
-    from core.models import Submission
     import ever_given.wrapper
 
+    from core.models import (
+        Evaluation,
+        FloatValue,
+        InputElement,
+        Prediction,
+        Submission,
+        SubmissionRun,
+    )
+
     print("running")
+
     submission = Submission.objects.get(pk=submission_id)
     container = submission.container
+    challenge = submission.challenge
+    # This should be passed in as the element
+    input_element = InputElement.objects.get(
+        name="benzene",
+        challenge=challenge
+    )
+
+    if not container.digest:
+        container.digest = "DEADBEEF"
+        container.save()
+    submission_run = SubmissionRun.objects.create(
+        submission=submission,
+        digest=container.digest,
+        is_public=True,
+        status=SubmissionRun._Status.PENDING,
+    )
+
+    evaluation = Evaluation.objects.create(
+        input_element=input_element, submission_run=submission_run, exit_status=0
+    )
+
     container_uri = f"{container.registry}/{container.label}:{container.tag}"
-    command = submission.challenge.execution_options_json["command"]
+    try:
+        command = submission.challenge.execution_options_json["command"]
+        command += element
+    except KeyError:  # if no execution options nothing to prepend
+        command = element
     print(command)
     print(container_uri)
     result = ever_given.wrapper.run_submission_container(container_uri, command)
-    return result
+    result = float(result.strip())
+    result_obj = FloatValue.objects.create(value=result)
+    #prediction = Prediction.objects.create(
+    #    challenge=challenge,
+    #    key="molWeight",
+    #    content_type=FloatValue,
+    #    evaluation=evaluation,
+    #)
+
+    submission_run._Status.SUCCESS
+    submission_run.save()
+    return (element, result)
