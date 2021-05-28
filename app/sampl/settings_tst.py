bc52e637bd1ed5698557a8237a4ed2178b8d51ce
Braxton Robbason <braxton.robbason@gmail.com>
2021-05-28 17:28:14 -0400

    address dask worker tests

--- app/sampl/settings_tst.py
+++ app/sampl/settings_tst.py
@@ -1,8 +1,11 @@
 from sampl.settings_dev import *  # lgtm [py/polluting-import]
 
-DATABASES["default"] = {
-    "ENGINE": "django.db.backends.postgresql_psycopg2",
-    "NAME": "test_sampl",
-    "USER": "sampl",
-    "HOST": "localhost"
-}
+DATABASES["default"]["NAME"] = "test_sampl"
+
+#{
+#    "ENGINE": "django.db.backends.postgresql_psycopg2",
+#    "NAME": "test_sampl",
+#    "USER": "sampl_user",
+#    "PASSWORD": "sampl_user"
+#    "HOST": "localhost"
+#}
