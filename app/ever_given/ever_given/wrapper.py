62f0c3b86b7354cc4c834c9b5daa43147517c4c4
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-06-17 11:04:49 -0700

    going to install ever_given from pypi

--- app/ever_given/ever_given/wrapper.py
+++
@@ -1,7 +0,0 @@
-import docker
-
-
-def run_submission_container(container_uri, command):
-    client = docker.from_env()
-    result = client.containers.run(container_uri, command, auto_remove=True)
-    return result
