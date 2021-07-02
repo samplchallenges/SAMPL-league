248055baaec2c659029ee056e824fbc1e504e840
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-07-02 12:31:37 -0700

    formatting fix

--- app/sampl/settings_prod.py
+++ app/sampl/settings_prod.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = [
     "samplmvp-env.eba-rhcwa63p.us-east-2.elasticbeanstalk.com",
 ]
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = "sampl-league-storage"
