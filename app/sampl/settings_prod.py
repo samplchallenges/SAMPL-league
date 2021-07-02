c8d92e5752f6fb93a58529d8aac280836fc31076
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-07-02 12:37:18 -0700

    just incase there are some default envars, lets add _S3 for our creds

--- app/sampl/settings_prod.py
+++ app/sampl/settings_prod.py
@@ -10,8 +10,8 @@ ALLOWED_HOSTS = [
 ]
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
-AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID_S3"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY_S3"]
 AWS_STORAGE_BUCKET_NAME = "sampl-league-storage"
 AWS_S3_REGION_NAME = "us-east-2"
 
