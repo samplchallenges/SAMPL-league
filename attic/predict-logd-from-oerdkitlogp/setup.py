ba24808457daa3a69883c7b7ab68769ba2e615bb
Meg Osato <megosato@Admins-MacBook-Pro.local>
2021-07-09 14:46:06 -0700

    fixes conda run issue

--- attic/predict-logd-from-oerdkitlogp/setup.py
+++ attic/predict-logd-from-oerdkitlogp/setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='oechem-rdkit-logd',
     version='0.1',
-    py_modules=['print_logd'],
+    py_modules=['print_logD'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        print-LogD=print_logd:get_logd
+        print-LogD=print_logD:get_logd
     ''',
 )
--- attic/predict-rdkitlogp/setup.py
+++ attic/predict-rdkitlogp/setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='rdkit_logp',
+    name='rdkit_logP',
     version='0.1',
-    py_modules=['print_logp'],
+    py_modules=['print_logP'],
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        print-LogP=print_logp:get_logp
+        print-LogP=print_logP:get_logp
     ''',
 )
