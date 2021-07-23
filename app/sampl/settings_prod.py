e0b04898fed39242c4bf62b91cc2fcce1bd48d89
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-07-23 12:39:36 -0700

    adds s3 storage for media files (#69)
    * adds s3 storage for media files
    
    * formatting fix
    
    * just incase there are some default envars, lets add _S3 for our creds

--- app/sampl/settings_prod.py
+++ app/sampl/settings_prod.py
@@ -9,6 +9,12 @@ ALLOWED_HOSTS = [
     "samplmvp-env.eba-rhcwa63p.us-east-2.elasticbeanstalk.com",
 ]
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID_S3"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY_S3"]
+AWS_STORAGE_BUCKET_NAME = "sampl-league-storage"
+AWS_S3_REGION_NAME = "us-east-2"
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
