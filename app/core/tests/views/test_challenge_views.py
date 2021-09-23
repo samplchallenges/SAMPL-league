6a2eec8ebcd00ea2974ef7d14881fe036e3a96c7
Mike Henry <11765982+mikemhenry@users.noreply.github.com>
2021-09-23 10:08:13 -0700

    remove unused arguments

--- app/core/tests/views/test_challenge_views.py
+++ app/core/tests/views/test_challenge_views.py
@@ -11,7 +11,7 @@ def test_challenge_detail(client, user, benzene_from_mol):
 
 
 @pytest.mark.django_db
-def test_challenge_list(client, user, benzene_from_mol):
+def test_challenge_list(client, user):
     client.force_login(user)
     response = client.get("/challenge/")
     assertContains(response, "molfile_molw")
