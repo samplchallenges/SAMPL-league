705810d4c7a53ed79bf0703d6310788f6d6bc4c5
megosato <osatom@uci.edu>
2021-10-27 15:32:44 -0700

    add mock timezone now fx

---
+++ app/core/tests/mocktime.py
@@ -0,0 +1,14 @@
+from datetime import datetime, timezone
+
+
+#start_at time is 2020, 1, 1, hour=1, tzinfo=timezone.utc
+#end_at time is 2020, 9, 1, hour=1, tzinfo=timezone.utc
+
+def active():
+    return datetime(2020, 7, 1, hour=1, tzinfo=timezone.utc)
+
+def inactive_before():
+    return datetime(2019, 1, 1, hour=1, tzinfo=timezone.utc)
+
+def inactive_after():
+    return datetime(2020, 9, 2, hour=1, tzinfo=timezone.utc)
