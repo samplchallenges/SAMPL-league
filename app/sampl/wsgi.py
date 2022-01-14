8b52e1fa65f362397970cb1e8b537c209bbf7e5d
Braxton Robbason <braxton.robbason@gmail.com>
2022-01-14 16:19:01 -0500

    External RDS: WIP

--- app/sampl/wsgi.py
+++ app/sampl/wsgi.py
@@ -7,9 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+import json
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+with open("/etc/sampl_rds_config.json", "r") as fp:
+    rds_config = json.load(fp)
+
+for key, value in rds_config.items():
+    os.environ.setdefault(key, value)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sampl.settings_prod")
 
