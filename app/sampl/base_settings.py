b6ddf859c46b797bed1b89af261f0cba64245805
megosato <osatom@uci.edu>
2022-02-04 12:32:13 -0800

    Revert "copy aws config files from s3 bucket to app (#227)"
    This reverts commit 7216235cd48a8352dbc3a1d637c2b3041cec713f.

--- app/sampl/base_settings.py
+++ app/sampl/base_settings.py
@@ -104,18 +104,13 @@ LOGIN_TO_AWS = False
 
 
 def run_aws_login():
-    login_password = subprocess.run(
-        [
-            "aws ecr get-login-password --no-include-email --region us-east-2 --profile sampl_pull"
-        ],
+    login_command = subprocess.run(
+        ["aws ecr get-login --no-include-email --region us-east-2"],
         shell=True,
         capture_output=True,
         check=True,
     )
-    login_command = "docker login --username AWS 103125031445.dkr.ecr.us-east-2.amazonaws.com --password {}".format(
-        login_password
-    )
-    subprocess.run([login_command], shell=True, check=True)
+    subprocess.run(login_command.stdout, shell=True, check=True)
 
 
 AWS_LOGIN_FUNCTION = run_aws_login
