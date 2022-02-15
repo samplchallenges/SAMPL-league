9b54b07bd60d18f7b4e785fb2be39fefea5a136c
megosato <osatom@uci.edu>
2022-02-15 08:36:20 -0800

    test

--- app/referee/tests/test_tasks.py
+++ app/referee/tests/test_tasks.py
@@ -224,7 +224,7 @@ def test_run_files(
     print(submission_run.evaluation_set.all())
     assert evaluation.status == models.Status.SUCCESS
     # prediction = evaluation.prediction_set.get()
-    print(evaluation.prediction_set.all())
+    print(evaluation.prediction_set.all().filter("molWeight"))
     assert prediction.value == pytest.approx(78.046950192)
 
 
