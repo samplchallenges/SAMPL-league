3d8c37865edbc7516d6f72585a16b11027e73798
Meghan Teruko Osato <osatom@uci.edu>
2022-02-21 15:32:52 -0800

    update slurm script

--- app/start_remote_scheduler.sh
+++ app/start_remote_scheduler.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
-#SBATCH --account=DMOBLEY_LAB
+#SBATCH --account=osatom
 #SBATCH --time=01:00:00
 #SBATCH --mem-per-cpu=1000
 #SBATCH --mail-type=FAIL
