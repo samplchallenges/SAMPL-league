13d70f0b6fe9af15d56da9228a7a2fdad2bb3d04
vagrant <vagrant@vagrant.vm>
2022-03-16 05:27:07 +0000

    test_cancellation pull singularity/docker ahead of time so pull time doesn't affect test case

--- ever_given/ever_given/tests/test_cancellation.py
+++ ever_given/ever_given/tests/test_cancellation.py
@@ -7,18 +7,25 @@ import shlex
 import ever_given.wrapper
 from ever_given.log_processing import QUEUE_WAIT_SECONDS, CancelledException
 
-
-
 @pytest.mark.parametrize(["container_engine"], [["docker"], ["singularity"]])
 def test_cancellation(container_engine):
     # Use a slow container so we have time to cancel it
     container_uri = "ghcr.io/megosato/logging-example:latest"
+    container_type = "docker"
     if container_engine == "singularity":
         command = f"singularity pull docker://{container_uri}"
-        subprocess.Popen(shlex.split(command), shell=True)
+        proc = subprocess.Popen(command, shell=True)
+        proc.wait()
         container_uri = "logging-example_latest.sif"
+        container_type = "singularity"
+    if container_engine == "docker":
+        command = f"docker pull {container_uri}"
+        proc = subprocess.Popen(command, shell=True)
+        proc.wait()
     kwargs = {"smiles": "c1cccnc1"}
+
     start_at = time.time()
+
     with pytest.raises(CancelledException):
         results = {
             key: value
@@ -26,7 +33,7 @@ def test_cancellation(container_engine):
                 container_uri,
                 kwargs=kwargs,
                 file_kwargs={},
-                container_type="docker",
+                container_type=container_type,
                 engine_name=container_engine,
                 cancel_requested_func=lambda: True,
             )
@@ -35,6 +42,8 @@ def test_cancellation(container_engine):
     # Should cancel in not much more time than QUEUE_WAIT_SECONDS * 2
     # in log_processing
     assert end_at - start_at < QUEUE_WAIT_SECONDS * 2 + 2
+
+
 
 @pytest.mark.parametrize(["container_engine"], [["docker"], ["singularity"]])
 def test_no_cancellation(container_engine):
