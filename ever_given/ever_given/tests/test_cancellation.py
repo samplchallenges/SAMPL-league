babeaebb5e1f3984c0ab4d2ccadf9c9e406c2591
vagrant <vagrant@vagrant.vm>
2022-03-16 04:27:25 +0000

    update tests

--- ever_given/ever_given/tests/test_cancellation.py
+++ ever_given/ever_given/tests/test_cancellation.py
@@ -20,7 +20,11 @@ def test_cancellation(container_engine):
         container_uri = "logging-example_latest.sif"
         container_type = "singularity"
         import os
-        os.path.exists("logging-example_latest.sif")
+        print(os.path.exists("logging-example_latest.sif"))
+    if container_engine == "docker":
+        command = f"docker pull {container_uri}"
+        subprocess.Popen(shlex.split(command), shell=True)
+
     kwargs = {"smiles": "c1cccnc1"}
 
     start_at = time.time()
