fafb18643e7dd0346d4a01db58869f08daa5e5ee
Braxton <robbason@users.noreply.github.com>
2022-04-08 16:00:36 -0400

    Return singularity output line by line (#288)

--- ever_given/ever_given/engines/singularity_engine.py
+++ ever_given/ever_given/engines/singularity_engine.py
@@ -10,6 +10,7 @@ from .utils import ContainerInstance, Engine, GUEST_OUTPUT_DIR
 AFTER_TERMINATE_WAIT = 5  # Seconds to wait after 'terminate' signal before 'kill'
 SINGULARITY_CONTAINER_TYPES = ["docker", "singularity_remote", "singularity_local"]
 
+
 class SingularityContainerInstance(ContainerInstance):
     def __init__(self, process: subprocess.Popen, container_uri):
         self.process = process
@@ -26,7 +27,7 @@ class SingularityContainerInstance(ContainerInstance):
         else:
             raise ValueError("Can't have want_stdout and want_stderr both false")
 
-        # FIXED: return pipe.readline() 
+        # FIXED: return pipe.readline()
         #   * returns an integer that needed to be decoded .decode('utf-8')
         #   * only returned the first line
         log_string_list = typing.cast(typing.List[str], [])
@@ -35,8 +36,7 @@ class SingularityContainerInstance(ContainerInstance):
             line = typing.cast(bytes, pipe.readline())
             if not line:
                 break
-            log_string_list.append(line.decode('utf-8'))
-        return log_string_list
+            yield line.decode("utf-8")
 
     def reload(self):
         pass  # don't need this for status, right?
@@ -49,15 +49,15 @@ class SingularityContainerInstance(ContainerInstance):
             self.process.kill()
 
     def remove(self):
-        pass 
-        #raise Exception("Not yet implemented")
+        pass
+        # raise Exception("Not yet implemented")
         # self.container.remove()
 
-        #container_sif = os.path.basename(self.container_uri)
-        #command = f"singularity cache clean "
-        #this removes the entire cache, singularity 3.7.2 does not have a --name option
+        # container_sif = os.path.basename(self.container_uri)
+        # command = f"singularity cache clean "
+        # this removes the entire cache, singularity 3.7.2 does not have a --name option
         # to remove specific containers
-         
+
     def status(self):
         return self.process.poll()
 
@@ -78,17 +78,18 @@ class SingularityEngine(Engine):
     ):
         # Check the container type
         if container_type not in SINGULARITY_CONTAINER_TYPES:
-            raise ValueError(f"Container type {container_type} not supported by singularity engine")
+            raise ValueError(
+                f"Container type {container_type} not supported by singularity engine"
+            )
         if aws_login_func:
             aws_login_func("singularity")
         bind_volumes = []
         for inputdir, guest_input_dir in inputdir_map.items():
-            bind_volumes.append(f"{inputdir}:{guest_input_dir}") #:ro")
+            bind_volumes.append(f"{inputdir}:{guest_input_dir}")  #:ro")
         if output_dir:
             output_dir = Path(output_dir).resolve()
-            bind_volumes.append(f"{output_dir}:{GUEST_OUTPUT_DIR}") #:rw")
+            bind_volumes.append(f"{output_dir}:{GUEST_OUTPUT_DIR}")  #:rw")
             command_list.extend(["--output-dir", GUEST_OUTPUT_DIR])
-
 
         command = _build_singularity_command(bind_volumes, container_uri, command_list)
         process = subprocess.Popen(
@@ -107,17 +108,20 @@ class SingularityEngine(Engine):
         else:
             raise Exception("Container Type Not Implemented")
 
+
 def _build_singularity_command(bind_volumes, container_uri, command_list):
     #  TODO: how to decide when to load Nvidia drivers?
-    singularity_cmd = ["singularity", "run",]
+    singularity_cmd = [
+        "singularity",
+        "run",
+    ]
     if bind_volumes and len(bind_volumes) >= 1:
         singularity_cmd.append("--bind")
         bind_str = ""
         for bind in bind_volumes:
             bind_str += bind + ","
         singularity_cmd.append(bind_str[:-1])
-    
-    singularity_cmd.extend([container_uri] + command_list)
-    
-    return singularity_cmd
 
+    singularity_cmd.extend([container_uri] + command_list)
+
+    return singularity_cmd
