acb2789a5c620dd6c03fce57409a244e0a49d988
Braxton Robbason <braxton.robbason@gmail.com>
2022-04-20 16:42:23 -0400

    WIP: fabfile

---
+++ deploy_files/install_certbot.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+yum-config-manager --enable epel*
+yum install -y certbot python2-certbot-nginx
