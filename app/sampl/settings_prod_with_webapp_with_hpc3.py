8833c8a3cf14d9f234d9677492c2dfb93a23edc9
Meghan Teruko Osato <osatom@uci.edu>
2022-04-20 10:22:12 -0700

    remove unncessary file

--- app/sampl/settings_prod_with_webapp_with_hpc3.py
+++
@@ -1,9 +0,0 @@
-# pylint: skip-file
-from .settings_prod import *  # lgtm [py/polluting-import]
-
-BASE_DIR = Path("/opt/app/sampl")
-MEDIA_ROOT = BASE_DIR / "media"
-
-# Settings file for webapp (running on AWS) when the
-# dask workers and scheduler will be on HPC3
-REMOTE_SCHEDULER = True
