8e5a0a45f328756fc194125b064006ffa82cd515
megosato <osatom@uci.edu>
2022-05-18 11:15:39 -0700

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -51,13 +51,13 @@ def deploy(c):
             remote_c.run("ls -l .")
 
 
-def _install_venv(remote_c):
-    with remote_c.cd("/var/app/current"):
-        remote_c.run("pipenv install")
-        remote_c.run("pipenv run pip uninstall ever-given")
-        remote_c.run("pipenv run pip install ../ever_given-0.0.13-py3-none-any.whl")
-        remote_c.run("pipenv run pip install dist/sampl_app-0.0.1-py3-none-any.whl")
-        remote_c.run("pipenv run pip freeze")
+# def _install_venv(remote_c):
+#     with remote_c.cd("/var/app/current"):
+#         remote_c.run("pipenv install")
+#         remote_c.run("pipenv run pip uninstall ever-given")
+#         remote_c.run("pipenv run pip install ../ever_given-0.0.13-py3-none-any.whl")
+#         remote_c.run("pipenv run pip install dist/sampl_app-0.0.1-py3-none-any.whl")
+#         remote_c.run("pipenv run pip freeze")
 
 def _download_aws_ecr_cred(c):
     with sampl_staging() as remote_c:
@@ -74,23 +74,78 @@ def _set_env_variables(c):
         remote_c.run("echo source /home/ec2-user/set_staging.sh >> ~/.bashrc")
         remote_c.run("source ~/.bashrc")
 
+def _upload_dependency_install_file(remote_c, filename):
+    if not exists(remote_c, "deploy_files"):
+        remote_c.run("mkdir deploy_files")
+    filepath = Path("deploy_files") / filename
+    remote_c.put(filepath, remote=str(filepath))
+
+def _run_install_file(remote_c, sh_file, sudo=False):
+    deploy_files = Path("deploy_files")
+    cmd = f"sudo bash {deploy_files / sh_file}" if sudo else f"bash {deploy_files / sh_file}" 
+    remote_c.run(cmd)
+
+def _install_dependency(install_file):
+    with sampl_staging() as remote_c:
+        _upload_dependency_install_file(remote_c, install_file)
+        _run_install_file(remote_c, install_file)
+
+@task
+def install_singularity(c):
+    install_file = "install_singularity.sh"
+    _install_dependency(install_file)
+
+@task 
+def install_python38(c):
+    install_file = "install_python38.sh"
+    _install_dependency(install_file)
+
+@task
+def install_docker(c):
+    install_file = "install_docker.sh"
+    _install_dependency(install_file)
+
+@task
+def install_certbot(c):
+    install_file = "logging.sh"
+    _install_dependency(install_file)
+
+@task
+def open_https_port(c):
+    install_file = "open_https_port.sh"
+    _install_dependency(install_file)
+
+@task
+def grant_executable_rights(c):
+    install_file = "grant_executable_rights.sh"
+    _install_dependency(install_file)
+
+@task
+def renew_ssl_certificate_cron_job(c)
+    install_file = "renew_ssl_certificate_cron_job.sh"
+    _install_dependency(install_file)
+
+@task
+def setup_logging(c):
+    install_file = "logging.sh"
+    _install_dependency(install_file)
+
+
+@task
+def setup_media_root(c):
+    install_file = "media_root.sh"
+    _install_dependency(install_file)
+
+
 
 @task
 def install_dependencies(c):
     with sampl_staging() as remote_c:
-        deploy_files = Path(DEPLOY_DIR)
-        if not exists(remote_c, "deploy_files"):
-            remote_c.run("mkdir deploy_files")
-        for filename in deploy_files.iterdir():
-            print("file:", filename)
-            remote_c.put(filename, remote=str(filename))
-
         def run_install_file(sh_file, sudo):
             cmd = f"sudo bash {deploy_files / sh_file}" if sudo else f"bash {deploy_files / sh_file}" 
             remote_c.run(cmd)
-
         _set_env_variables(c)
-
+        run_install_file("install_venv.sh", False)
         #run_install_file("install_python38.sh", True)
         #run_install_file("install_certbot.sh", True)
         #run_install_file("open_https_port.sh", True)
@@ -100,5 +155,4 @@ def install_dependencies(c):
         #run_install_file("logging.sh", True)
         #run_install_file("media_root.sh", True)
         #run_install_file("install_singularity.sh", True)
-        #_install_venv(remote_c)
     #_download_aws_ecr_cred(c)
