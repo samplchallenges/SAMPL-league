7e0486c2b49e47631ab4584369c560e0500590f9
megosato <osatom@uci.edu>
2022-05-19 10:22:44 -0700

    split fabfile into steps

--- deploy_files/install_certbot.sh
+++ deploy_files/install_certbot.sh
@@ -3,13 +3,13 @@
 # from app/.ebextensions/00_install_certbot.config
 
 # 00_download_epel
-wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
+sudo wget -r --no-parent -A 'epel-release-*.rpm' http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/
 
 # 10_install_epel_release
-rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
+sudo rpm -Uvh dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-*.rpm
 
 # 20_enable_epel
-yum-config-manager --enable epel*
+sudo yum-config-manager --enable epel*
 
 # 30_install_certbot
-yum install -y certbot python2-certbot-nginx
+sudo yum install -y certbot python2-certbot-nginx
